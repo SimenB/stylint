b6b73d5a04482aeefc5ca8f48fcbe02d54aeea70
Ross <me@rosspatton.com>
2014-12-06 17:07:32 -0800

    recursively readin dirs and outputtin errors

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
+/**
+ * Stylus Lint (splinter) (the p is silent)
+ * @description A basic, configurable, node based, stylus linter cli
+ * @todo everything, basically, mostly as follows:
+ *       1. check for semicolons @DONE
+ *       2. check for colons @DONE
+ *       3. check for space after comment @DONE
+ *       4. check for 0px @DONE
+ *       5. check for tab depth (partially implemented, needs bulletproofing)
+ *       6. check for * selector (partially implemented, needs bulletproofing)
+ *       7. check for spaces vs tabs (error out if using spaces, or tabs, depending on config) @NOT DONE
+ *       8. check for 0 0 0 0 or 50px 50px 50px type mistakes @NOT DONE
+ *       9. check for alphabetical order @NOT DONE
+ *       10. check for duplicate selectors @NOT DONE
+ *       11. check for valid properties @NOT DONE
+ *       11. make configurable via .json @NOT DONE
+ *       12. accept cli flags (like --force) @NOT DONE
+ */
+
+
 // modules
 const   fs          = require('fs'),
         chalk       = require('chalk'),
         fileinput   = require('fileinput'),
         stream      = require('stream'),
         readline    = require('readline'),
+        lineReader 	= require('line-reader'),
+        lazy		= require('lazy'),
         _           = require('lodash');
 
 
 // regexes
 const   comment = /\/\/\s/,                 // check for space after comment line
-        starS   = /(\* )/,                 // check for space after star (inside a docblockr comment)
+        starS   = /(\* )/,                 	// check for space after star (inside a docblockr comment)
         starO   = /(\/\*\*)/ || /(\/\*)/,   // check for star selector as opening comment
-        starE   = /(\*\/)/;                 // finally, check for star selector as closing comment
+        starE   = /(\*\/)/,                 // finally, check for star selector as closing comment
+        tabs	= /^(\t)*/,					// count indents
+        amp 	= /^(\&)/,					// check if using & selector before we count tabs
+        spaces	= /( ){4}\D\W+/g;			// if using spaces for indents, check consistency
+
 
 // other stuff
 var errors = [],
@@ -20,100 +46,120 @@ var errors = [],
     count = 0;
 
 
-fileinput.input('test/test.styl').on('line', function(line) {
-    var line = line.toString();
-    count += 1;
+// recursively walk a dir, get files
+var walk = function(dir, done) {
+	var results = [];
 
-    // check for comment
-    if (line.indexOf('//') !== -1) {
-        // check for space after comment, if no space, return warning
-        if (!comment.test(line)) {
-            warnings.push('space after comment is preferred: \n' + count + ': ' + line);
-        }
-    }
+	fs.readdir(dir, function(err, list) {
+		if (err) return done(err);
+		var i = 0;
 
-    // check for 0px
-    if (line.indexOf(' 0px') !== -1) {
-        warnings.push('0 is preferred over 0px. Found on line: \n' + count + ': ' + line)
-    }
+		(function next() {
+			var file = list[i++];
 
-    /**
-     * check for * selector. (super basic atm, @TODO needs to not trigger on regex selectors)
-     * technically this is used as part of resets often, for good reason, despite its slowness
-     * which is why i'm setting it up as a warning as it won't break code but you might prefer to not use it
-     */
-    if (line.indexOf('*') !== -1) {
-    	// check for various places where the * is valid (just comment checks atm)
-        if (!starO.test(line) && !starE.test(line) && !starS.test(line)) {
-            warnings.push('* selector is very slow. Found on line: \n' + count + ': ' + line);
-        }
-    }
+			if (!file) return done(null, results);
 
-    // check for semicolons
-    if (line.indexOf(';') !== -1) {
-        errors.push('semicolon found on line: \n' + count + ': ' + line)
-    }
-}).
-on('error', function(err) {
-    throw err;
-}).
-on('end', function() {
-    for (var i = 0; i < warnings.length; i++) {
-        console.log( chalk.yellow('Warning: ' + warnings[i]) );
-    }
-    for (var i = 0; i < errors.length; i++) {
-        console.log( chalk.red('Error: ' + errors[i]) );
-    }
+			file = dir + '/' + file;
+
+			fs.stat(file, function(err, stat) {
+				if (stat && stat.isDirectory()) {
+					walk(file, function(err, res) {
+						results = results.concat(res);
+						next();
+					});
+				}
+				else {
+					results.push(file);
+					next();
+				}
+			});
+		})();
+	});
+};
+
+
+walk('test', function(err, files) {
+	if (err) throw err;
+
+	fileinput.input(files).on('line', function(line) {
+	  	console.log('im doing something');
+	    var line = line.toString(),
+	    	output = line.trim();
+
+		count += 1;
+
+		// check comment style correctness
+		if (line.indexOf('//') !== -1) {
+		    // check for space after comment, if no space, return warning
+		    if (!comment.test(line)) {
+		        warnings.push('Space after comment is preferred:\nLine: ' + count + ': ' + output);
+		    }
+		}
+
+	    // check for 0px
+	    if (line.indexOf(' 0px') !== -1) {
+	        warnings.push('0 is preferred over 0px.\nLine: ' + count + ': ' + output)
+	    }
+
+	    /**
+	     * check for * selector. (super basic atm, @TODO needs to not trigger on regex selectors)
+	     * technically this is used as part of resets often, for good reason, despite its slowness
+	     * which is why i'm setting it up as a warning as it won't break code but you might prefer to not use it
+	     */
+	    if (line.indexOf('*') !== -1) {
+	    	// check for various places where the * is valid (just comment checks atm)
+	        if (!starO.test(line) && !starE.test(line) && !starS.test(line)) {
+	            warnings.push('* selector is slow. Consider a different selector.\nLine: ' + count + ': ' + output);
+	        }
+	    }
+
+	    // check for colons
+	    if (line.indexOf(': ') !== -1) {
+	        warnings.push('Unecessary colon found:\nLine: ' + count + ': ' + output)
+	    }
+
+	    // if using spaces, check for consistent indentation (in this case, 4 spaces per indent)
+	    // also check that line is not commented out
+	    if (!_.isNull(line.match(spaces)) && line.indexOf('/') === -1) {
+	    	console.log(line.match(spaces).length);
+	    	console.log('Indentation not consistent:\nLine: ' + count + ': ' + output);
+			// if (!line.match(spaces.length % 4)) {
+			// 	warnings.push('Indentation not consistent:\nLine: ' + count + ': ' + output);
+			// }
+	    }
+
+	    // check for semicolons
+	    if (line.indexOf(';') !== -1) {
+	        errors.push('Unecessary semicolon found:\nLine: ' + count + ': ' + output)
+	    }
+
+	    /**
+	     * issue error if nesting is greater than 3 and not starting with ampersand
+	     * stuff like :hover for instance, shouldn't count towards selector depth @TODO finetune this
+	     * also stuff like @media shouldn't count either @TODO
+	     */
+	    if (line.match(tabs)[0].length > 4 && !amp.test(line)) {
+	    	errors.push('Selector depth greater than 4:\nLine: ' + count + ': ' + output);
+	    }
+	}).
+	on('error', function(err) {
+	    throw err;
+	}).
+	on('end', function() {
+
+		// if (errors.length > 3) {
+		// 	console.error(chalk.bold.red('Splinter: Too many errors. Aborting.'));
+		// }
+		// else {
+		    for (var i = 0; i < warnings.length; i++) {
+		        console.log( chalk.yellow('Warning: ' + warnings[i]) + '\n' );
+		    }
+		    for (var i = 0; i < errors.length; i++) {
+		        console.log( chalk.red('Error: ' + errors[i]) + '\n' );
+		    }
+
+		    console.log(chalk.blue('Total Warnings: ' + warnings.length));
+		    console.log(chalk.blue('Total Errors: ' + errors.length));
+		// }
+	});
 });
-
-
-// fs.readFile('test/test.styl', 'utf8', function(err, data) {
-//     if (err) {
-//         return console.error(err);
-//     }
-
-//     // console.log(data);
-//     // console.log(typeof data);
-//     console.log(semi.exec(data))
-
-//     if (!_.isNull(semi.exec(data))) {
-//         console.log('Match!');
-//     }
-//     else {
-//         console.log('No Match!');
-//     }
-// });
-
-// fileinput.input().on('line', function(line) {
-//     console.log( fileinput.lineno(), line.toString('utf8') );
-// });
-
-
-// var instream = fs.createReadStream(process.argv[2]);
-// var outstream = new stream;
-// outstream.readable = true;
-// outstream.writable = true;
-
-// var rl = readline.createInterface({
-//     input: instream,
-//     output: outstream,
-//     terminal: false
-// });
-
-// rl.on('line', function(line) {
-//     console.log(line);
-//     //Do your stuff ...
-//     //Then write to outstream
-//     // rl.write(cubestuff);
-// }).
-// on('error', function(err) {
-//     throw err;
-// });
-
-// fs.readdir(process.argv[2], function(err, contents) {
-//     if (err) throw err;
-
-//     contents.forEach(function(file) {
-//         files.push(file);
-//     });
-// });
