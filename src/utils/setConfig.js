88b51cf17e82fc0c7c39d90d6d76bd9287ad54ad
Ross <me@rosspatton.com>
2015-06-21 23:13:29 -0700

    more config options

--- src/utils/setConfig.js
+++ src/utils/setConfig.js
@@ -10,17 +10,18 @@ var pathIsAbsolute = require( 'path-is-absolute' );
  * 2: user passes location of .stylintrc file to use
  * 3: user has a .stylintrc file in their dir but doesnt pass anything
  * 4: none of the above, fallback to initial config
- * @param {string} [potentialPath] location of our new config
  * @returns {Function} kick off linter again
 */
 var setConfig = function() {
+	var files = [];
 	var path = '';
+	var returnConfig = this.config;
 	var potentialPath = '';
 	var flag = process.argv.indexOf( '--config' ) !== -1 || process.argv.indexOf( '-c' ) !== -1;
 
 	// if 1, the customConfig will be what we want
 	if ( this.customConfig ) {
-		return this.customConfig;
+		returnConfig = this.customConfig;
 	}
 	// if 2
 	else if ( flag ) {
@@ -32,11 +33,29 @@ var setConfig = function() {
 		}
 
 		path = pathIsAbsolute( potentialPath ) ? potentialPath : process.cwd() + '/' + potentialPath;
-		return JSON.parse( fs.readFileSync( path ) );
+		try {
+			returnConfig = JSON.parse( fs.readFileSync( path ) );
+		}
+		catch( err ) {
+			throw err;
+		}
+	}
+	// if 3
+	else {
+		try {
+			files = fs.readdirSync( process.cwd() );
+			if ( files.indexOf( '.stylintrc' ) !== -1 ) {
+				returnConfig = JSON.parse( fs.readFileSync( process.cwd() + '/.stylintrc' ) );
+			}
+		}
+		// in case theres an issue parsing
+		catch ( err ) {
+			throw err;
+		}
 	}
 
 	// 4, just return the initial config
-	return this.config;
+	return returnConfig;
 };
 
 module.exports = setConfig;
