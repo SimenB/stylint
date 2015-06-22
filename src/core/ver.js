eab31b71a913f55ef367401b0b73ddfb3bf0a824
Ross <me@rosspatton.com>
2015-06-22 02:17:04 -0700

    bump ver

--- src/core/ver.js
+++ src/core/ver.js
@@ -2,7 +2,7 @@
 
 // called when --version or -v flags used, just displays version number
 var ver = function() {
-	return console.log( '\nStylint version: 1.0.0\n' );
+	return console.log( '\nStylint version: 1.0.1\n' );
 };
 
 module.exports = ver;
