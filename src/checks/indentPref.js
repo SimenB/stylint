4cee46d07021d58e589e343e06d2135ddc22b7f1
rossPatton <me@rosspatton.com>
2015-08-07 15:15:44 -0700

    cleanup

--- src/checks/indentPref.js
+++ src/checks/indentPref.js
@@ -3,32 +3,16 @@
 
 /**
  * @description checks that the # of spaces used is consistent
- * @param {string} [line] curr line being linted
  * @returns {boolean} true if # of spaces correct, false if not
  */
-var indentPref = function( line ) {
+var indentPref = function() {
 	if ( typeof this.state.conf !== 'number' ) { return }
 
 	var indentCorrect = true
 
-	console.log( this.state.context )
-	console.log( this.state.context % 1 == 0 )
-
 	if ( this.state.context % 1 !== 0 ) {
 		indentCorrect = false
 	}
-
-	// if ( this.state.conf === '@extends' &&
-	// 	line.indexOf( '@extends ' ) === -1 ) {
-
-	// 	indentCorrect = true
-	// }
-	// // else @extend is your pref
-	// else if ( this.state.conf === '@extend' &&
-	// 	line.indexOf( '@extend ' ) === -1 ) {
-
-	// 	indentCorrect = true
-	// }
 
 	if ( indentCorrect === false ) {
 		this.msg( 'incorrect # of spaces for indent, use ' + this.state.conf )
