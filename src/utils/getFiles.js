e6f5f52fd7a8f63a12623d6449b95079dfa64937
Lance Miller <lmiller@bamboohr.com>
2015-11-04 15:31:21 -0700

    add config option for excluding file patterns

--- src/utils/getFiles.js
+++ src/utils/getFiles.js
@@ -3,6 +3,8 @@
 var fs = require( 'fs' )
 var glob = require( 'glob' )
 var async = require( 'async' )
+var path = require( 'path' )
+var Minimatch = require( 'minimatch' ).Minimatch
 
 /**
  * @description globs files and returns an array, used in various methods
@@ -13,9 +15,25 @@ var getFiles = function( dir ) {
 	if ( typeof dir !== 'string' ) {
 		throw new TypeError( 'getFiles err. Expected string, but received: ' + typeof dir )
 	}
+	
+	this.config.exclude = ( this.config.exclude || [] ).map( function( exclude ) {
+		return new Minimatch( exclude, {
+			matchBase: true
+		} )
+	} )
 
 	glob( dir, {}, function( err, files ) {
 		if ( err ) { throw err }
+		files = files.filter( function( file ) {
+			var excluded = false,
+				relPath = path.relative( dir.replace( '/**/*.styl', '' ), file )
+			
+			this.config.exclude.forEach( function( exclude ) {
+				excluded = excluded || exclude.match( relPath )
+			} )
+			
+			return !excluded
+		}, this )
 		this.cache.filesLen = files.length - 1
 		this.cache.files = files
 		return async.map( this.cache.files, fs.readFile, this.parse.bind( this ) )
