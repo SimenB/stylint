bca7fb49eb7eddf91c00e1b68b2215c3117e5c2f
Wojciech Czerniak <wojciech.czerniak@gmail.com>
2016-03-28 00:24:08 +0200

    add column number as second this.msg() parameter
    single place and style for column number setting, a lot cleaner
    solves order problem, column always is set before calling reporter

--- src/utils/msg.js
+++ src/utils/msg.js
@@ -3,10 +3,13 @@
 /**
  * @description basically just sets the severity and routes output to the reporter
  * @param {string} [str] outputted string from one of the checks
+ * @param {number} [column] column where error was found
  * @returns {Function} push formatted output to appropriate array
 */
-var msg = function( str ) {
+var msg = function( str, column ) {
 	var arr
+
+	this.cache.columnNo = typeof column !== 'undefined' ? column : -1; 
 
 	// determine which group the msg belongs to
 	arr = this.state.severity === 'Warning' ? this.cache.warnings : this.cache.errs
