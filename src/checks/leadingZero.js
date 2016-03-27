d4c7486bb4ca845fa13e247e604fe03d71bce5b6
Wojciech Czerniak <wojciech.czerniak@gmail.com>
2016-03-27 21:07:35 +0200

    fix leadingZero check to pass tests

--- src/checks/leadingZero.js
+++ src/checks/leadingZero.js
@@ -1,14 +1,14 @@
 'use strict'
 
-var decimalRe = /(?:^|[^a-z])\.\d/i
-var leadZeroRe = /(?:^|\D)0\.\d/
-var nonZeroRe = /(?:^|\D)\.\d/
+var decimalRe = /[^\d+](0+\.\d+)|[\s,\(](\.\d+)/i
+var leadZeroRe = /[^\d+](0+\.\d+)/
+var nonZeroRe = /[\s,\(](\.\d+)/
 
 
 /**
  * @description check for leading 0 on numbers ( 0.5 )
  * @param {string} [line] curr line being linted
- * @returns {boolean} true if mixed, false if not
+ * @returns {boolean|undefined} true if mixed, false if not
  */
 var leadingZero = function( line ) {
 	if ( !decimalRe.test( line ) ) { return }
@@ -20,7 +20,7 @@ var leadingZero = function( line ) {
 		this.msg( 'leading zeros for decimal points are required' )
 	}
 	else if ( this.state.conf === 'never' && leadZeroFound ) {
-		this.msg( 'leading zeros for decimal points are unecessary' )
+		this.msg( 'leading zeros for decimal points are unnecessary' )
 	}
 
 	return leadZeroFound
