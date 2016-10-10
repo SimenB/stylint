306c3097de8088567df3e0b99cadb08abf922930
Simen Bekkhus <sbekkhus91@gmail.com>
2016-10-10 20:27:14 +0200

    index

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-'use strict'
+'use strict';
 
 // our stampit modules
-var stampit = require( 'stampit' )
-var fs = require( 'fs' )
+const stampit = require('stampit');
+const fs = require('fs');
 
 // let there be light ( * )
 // basically, with stampit we take a bunch of different objects
@@ -14,7 +14,6 @@ var fs = require( 'fs' )
 // init() -> read() -> parse() -> lint() -> done()
 // init() -> watch() -> read() -> parse() -> lint() -> done()
 
-
 /**
  * main stylint kickoff function
  * @param {string} path   [custom path if used programmatically]
@@ -22,61 +21,58 @@ var fs = require( 'fs' )
  * @param {function} [callback] [a callback called just before exiting the process if not watching]
  * @return {Object} [the composed stylint object]
  */
-var Stylint = function( path, config, callback ) {
-	return stampit().compose(
-		require( './src/core/' ),
-		require( './src/checks/' ),
-		require( './src/state/' ),
-		stampit().enclose( function() {
-			var pkg = null
-			try {
-				pkg = require( process.cwd() + '/package.json' )
-			}
-			catch ( err ) {
-				// no output
-			}
+const stylint = function (path, config, callback) {
+  return stampit().compose(
+    require('./src/core/'),
+    require('./src/checks/'),
+    require('./src/state/'),
+    stampit().enclose(function () {
+      let pkg = null;
+      try {
+        // TODO: Use pkg-up
+        // eslint-disable-next-line import/no-dynamic-require
+        pkg = require(`${process.cwd()}/package.json`);
+      } catch (err) {
+        // no output
+      }
 
-			// set safe path defaults
-			if ( typeof path === 'undefined' ) {
-				this.state.path = './'
-			}
-			else if ( path instanceof Array || typeof path === 'string' ) {
-				this.state.path = path
-			}
+      // set safe path defaults
+      if (typeof path === 'undefined') {
+        this.state.path = './';
+      } else if (path instanceof Array || typeof path === 'string') {
+        this.state.path = path;
+      }
 
-			// look for a stylintignore array
-			// for ignoring specific files
-			// first look in package.json
-			// then look for .stylintignore in the main dir
-			if ( pkg !== null &&
-					typeof pkg.stylintignore !== 'undefined' &&
-					pkg.stylintignore instanceof Array ) {
-				this.state.exclude = pkg.stylintignore
-			}
-			else {
-				try {
-					var stylintIgnore = fs.readFileSync( process.cwd() + '/.stylintignore' )
-					this.state.exclude = stylintIgnore
-						.toString()
-						.split( '\n' )
-						.filter( function( d ) {
-							return d
-						} )
-				}
-				catch ( err ) {
-					// do no-thing
-				}
-			}
+      // look for a stylintignore array
+      // for ignoring specific files
+      // first look in package.json
+      // then look for .stylintignore in the main dir
+      if (pkg !== null &&
+        typeof pkg.stylintignore !== 'undefined' &&
+        pkg.stylintignore instanceof Array) {
+        this.state.exclude = pkg.stylintignore;
+      } else {
+        try {
+          const stylintIgnore = fs.readFileSync(`${process.cwd()}/.stylintignore`);
+          this.state.exclude = stylintIgnore
+            .toString()
+            .split('\n')
+            .filter(d => d);
+        } catch (err) {
+          // do no-thing
+        }
+      }
 
-			this.customConfig = typeof config === 'object' ? config : false
-			this.callback = callback || function() {}
-		} ).enclose( require( './src/core/init' ) )
-	)
-}
+      this.customConfig = typeof config === 'object' ? config : false;
+      this.callback = callback || function () {
+      };
+    }).enclose(require('./src/core/init'))
+  );
+};
 
-var api = function( config ) {
-	return Stylint().create( {}, { config: config } )
-}
+const api = function (config) {
+  return stylint().create({}, { config });
+};
 
-module.exports = Stylint
-module.exports.api = api
+module.exports = stylint;
+module.exports.api = api;
