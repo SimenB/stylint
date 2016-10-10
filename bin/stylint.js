6b97a9fc03e2a8fe6da149eac8bad62ff8a7c03b
Simen Bekkhus <sbekkhus91@gmail.com>
2016-10-10 20:21:35 +0200

    bin

--- bin/stylint.js
+++ bin/stylint.js
@@ -1,52 +1,50 @@
 #!/usr/bin/env node
 
-'use strict'
+'use strict';
 
-var yargs = require( 'yargs' )
-var stylint = require( '../' )
+const yargs = require('yargs');
+const stylint = require('../');
 
-var options = yargs
-	.usage( 'Usage: $0 [dir | file] ' )
-	.option( 'watch', {
-		alias: 'w',
-		describe: 'Watch file or directory and run lint on change',
-		type: 'boolean'
-	} )
-	.option( 'config', {
-		alias: 'c',
-		describe: 'Location of custom config file',
-		type: 'string'
-	} )
-	.option( 'strict', {
-		alias: 's',
-		describe: 'Run all tests, regardless of config',
-		type: 'boolean'
-	} )
-	.option( 'reporter', {
-		alias: 'r',
-		describe: 'Custom reporter npm module name',
-		type: 'string',
-		requiresArg: true
-	} )
-	.version( function() {
-		return 'Stylint version: ' + require( '../package' ).version
-	} )
-	.alias( 'version', 'v' )
-	.help( 'help' )
-	.alias( 'help', 'h' )
-	.alias( 'help', '?' )
-	.example( '$0 directory', 'Run Stylint on all .styl-files in "directory"' )
-	.epilogue( 'GPL-3.0 License' )
-	.argv
+const options = yargs
+  .usage('Usage: $0 [dir | file] ')
+  .option('watch', {
+    alias: 'w',
+    describe: 'Watch file or directory and run lint on change',
+    type: 'boolean',
+  })
+  .option('config', {
+    alias: 'c',
+    describe: 'Location of custom config file',
+    type: 'string',
+  })
+  .option('strict', {
+    alias: 's',
+    describe: 'Run all tests, regardless of config',
+    type: 'boolean',
+  })
+  .option('reporter', {
+    alias: 'r',
+    describe: 'Custom reporter npm module name',
+    type: 'string',
+    requiresArg: true,
+  })
+  .version(() => `Stylint version: ${require('../package').version}`)
+  .alias('version', 'v')
+  .help('help')
+  .alias('help', 'h')
+  .alias('help', '?')
+  .example('$0 directory', 'Run Stylint on all .styl-files in "directory"')
+  .epilogue('GPL-3.0 License')
+  .argv;
 
-var stylintInstance = stylint().create( {}, {
-	watch: options.watch,
-	config: options.config,
-	strict: options.strict,
-	reporter: options.reporter
-}, options._.length > 1 ? options._ : options._[0] )
+const stylintInstance = stylint().create({}, {
+  watch: options.watch,
+  config: options.config,
+  strict: options.strict,
+  reporter: options.reporter,
+}, options._.length > 1 ? options._ : options._[0]);
 
 // to allow instantiation without doing anything, manually call read
-if ( !options.watch ) {
-	stylintInstance.read()
+if (!options.watch) {
+  stylintInstance.read();
 }
