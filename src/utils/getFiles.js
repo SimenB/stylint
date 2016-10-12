2702dfb4648887b8c478e56f85bd2b95ed738cb4
Simen Bekkhus <sbekkhus91@gmail.com>
2016-10-12 18:13:40 +0200

    [WIP] Make use of glob utils from eslint

--- src/utils/getFiles.js
+++ src/utils/getFiles.js
@@ -1,9 +1,9 @@
 'use strict'
 
 var fs = require( 'fs' )
-var glob = require( 'glob' )
 var async = require( 'async' )
 var path = require( 'path' )
+var globUtil = require( '@simenb/eslint-file-util/lib/util/glob-util' )
 
 /**
  * @description globs files and returns an array, used in various methods
@@ -15,45 +15,33 @@ var getFiles = function( dir ) {
 		throw new TypeError( 'getFiles err. Expected string or array, but received: ' + typeof dir )
 	}
 
-	if ( typeof dir === 'string' ) {
-		return glob( dir, {}, function( err, files ) {
-			if ( err ) { throw err }
+	var arrayOfDirs = Array.isArray( dir ) ? dir : [dir]
 
-			files = files.filter( function( file ) {
-				var excluded = false
-				var relPath = path.relative( dir.replace( '/**/*.styl', '' ), file )
+	var globPatterns = globUtil.resolveFileGlobPatterns( arrayOfDirs, { extensions: ['.styl'] } )
+	var files = globUtil.listFilesToProcess( globPatterns )
 
-				this.config.exclude.forEach( function( exclude ) {
-					excluded = excluded || exclude.match( relPath )
-				} )
-
-				return !excluded
-			}, this )
-
-			this.cache.filesLen = files.length - 1
-			this.cache.files = files
-
-			return async.map( this.cache.files, fs.readFile, this.parse.bind( this ) )
-		}.bind( this ) )
-	}
-	else if ( dir instanceof Array ) {
-
-		var files = dir.filter( function( filepath ) {
+	files = files
+		.filter( function( file ) {
+			return !file.ignored
+		} )
+		.map( function( file ) {
+			return file.filename
+		} )
+		.filter( function( file ) {
 			var excluded = false
+			var relPath = path.dirname( file )
 
 			this.config.exclude.forEach( function( exclude ) {
-				excluded = excluded || exclude.match( filepath )
+				excluded = excluded || exclude.match( relPath )
 			} )
 
 			return !excluded
 		}, this )
 
-		this.cache.filesLen = files.length - 1
-		this.cache.files = files
-		return this.cache.files.forEach( function( file ) {
-			return this.read( file )
-		}.bind( this ) )
-	}
+	this.cache.filesLen = files.length - 1
+	this.cache.files = files
+
+	return async.map( this.cache.files, fs.readFile, this.parse.bind( this ) )
 }
 
 module.exports = getFiles
