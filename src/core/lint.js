476d523b92505d8be19767c9da7d8496ff033cb9
Simen Bekkhus <sbekkhus91@gmail.com>
2017-03-04 20:28:25 +0100

    Refactor looping through all checks

--- src/core/lint.js
+++ src/core/lint.js
@@ -1,30 +1,24 @@
 'use strict';
 
+const entries = require('object.entries');
+
 /**
  * @description Runs tests according to config (or all if strict is true).
  * @returns {Function | undefined} Undefined if just calling the method, function is linting over.
  */
 const lint = function() {
-  let method;
-  const checks = Object.getPrototypeOf(this).lintMethods;
-
-  // TODO: Fix this!
-  /* eslint-disable */
-  for (method in checks) {
-    if (checks.hasOwnProperty(method)) {
-      /* eslint-enable */
-      if (this.config[method]) {
-        // save config rule name for use in reporters
-        this.cache.rule = method;
-        // state.conf === 'always' || 'never' || etc
-        this.state.conf = this.config[method].expect || this.config[method];
-        // state.severity === 'error' || 'warning'
-        this.state.severity = this.config[method].error ? 'error' : 'warning';
-        // run the actual check against the line
-        checks[method].call(this, this.cache.line, this.cache.source);
-      }
-    }
-  }
+  entries(Object.getPrototypeOf(this).lintMethods).filter(check => this.config[check[0]]).forEach(check => {
+    const checkName = check[0];
+    const checkFunc = check[1];
+    // save config rule name for use in reporters
+    this.cache.rule = checkName;
+    // state.conf === 'always' || 'never' || etc
+    this.state.conf = this.config[checkName].expect || this.config[checkName];
+    // state.severity === 'error' || 'warning'
+    this.state.severity = this.config[checkName].error ? 'error' : 'warning';
+    // run the actual check against the line
+    checkFunc.call(this, this.cache.line, this.cache.source);
+  });
 
   // save our curr context so we can use it next time
   // this.cache.prevFile = this.cache.file
