566ec3c6e96126e1e0c0bd64f2b5527238139b9e
Emma Bukacek <emma.bukacek@gmail.com>
2017-04-14 02:50:57 -0500

    First crack at the default formatter. Time for :sleeping_bed:

--- src/formatters/default.js
+++ src/formatters/default.js
@@ -1,3 +1,7 @@
+'use strict';
+
+const _ = require('lodash');
+
 /**
  * @description Default formatter for stylint.
  * @param  {Object} report - Report object containing all results.
@@ -9,6 +13,35 @@ const defaultFormatter = function(report, options, kill) {
   if (report.results.length === 0) {
     return '';
   }
+
+  const {results, errorCount, maxErrors, maxWarnings, warningCount} = report;
+
+  let formattedMessages = results.map(result => {
+    const {filePath, messages} = result;
+
+    newMessages = messages.map(msg => {
+      const {column, line, ruleId, severity} = msg;
+      const lineData = column ? `${line}:${column}` : line;
+
+      const newMessage = `${severity}: ${msg}
+      File: ${filePath}
+      Line: ${lineData}`;
+
+      return _.set(msg, 'message', newMessage);
+    })
+
+    return _.set(result, newMessages);
+  });
+
+  response = `Stylint: ${errorCount} Errors.`;
+  response += maxErrors ? ` (Max Errors: ${maxErrors} )` : ''
+  response = `\nStylint: ${warningCount} Warnings.`;
+  response += maxWarnings ? ` (Max Warnings: ${maxWarnings} )` : ''
+
+  response += kill ? '\nStylint: Over Error or Warning Limit.';
+  response = `${formattedMessages}\n\n${response}`
+
+  return response;
 }
 
 module.exports = defaultFormatter;
@@ -39,7 +72,6 @@ module.exports = defaultFormatter;
 // 		return prettyReporter.call( this, msg )
 // 	}
 //
-// 	return this.state.severity + ': ' + msg + '\nFile: ' + this.cache.file + '\nLine: ' + this.cache.lineNo + ': ' + this.cache.origLine.trim()
 //
 // }
 //
