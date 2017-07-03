da3f6d8ba9f810e5eea0f1be4aef8de06767e517
Emma Bukacek <emma.bukacek@gmail.com>
2017-07-03 10:44:07 -0700

    Remove variable hoisting from default formatter

--- src/formatters/default.js
+++ src/formatters/default.js
@@ -10,22 +10,36 @@ const _ = require('lodash');
  * @returns {string} The formatted message.
  */
 const defaultFormatter = function(report, options, kill) {
-  if (report.results.length === 0) return '';
+  const results = report.results;
 
-  const {results, errorCount, maxErrors, maxWarnings, warningCount} = report;
+  if (results.length === 0) {
+    return '';
+  }
 
-  if (errorCount === 0 && warningCount === 0) return '';
+  const errorCount = report.errorCount;
+  const warningCount = report.warningCount;
 
-  let formattedMessages = results.map(result => {
-    const {filePath, messages} = result;
+  if (errorCount === 0 && warningCount === 0) {
+    return '';
+  }
 
-    newMessages = messages.map(msg => {
-      const {column, line, ruleId, severity} = msg;
+  const maxErrors = report.maxErrors;
+  const maxWarnings = report.maxWarnings;
+
+  const formattedMessages = report.results.map(result => {
+    const filePath = result.filePath;
+    const messages = result.messages;
+
+    const newMessages = messages.map(msg => {
+      const column = msg.column;
+      const line = msg.line;
+      const severity = msg.severity;
+
       const lineData = column ? `${line}:${column}` : line;
 
-      const newMessage = `${severity}: ${msg}
-      File: ${filePath}
-      Line: ${lineData}`;
+      newMessage = `${severity}: ${msg}
+        File: ${filePath}
+        Line: ${lineData}`;
 
       return _.set(msg, 'message', newMessage);
     })
@@ -33,7 +47,7 @@ const defaultFormatter = function(report, options, kill) {
     return _.set(result, newMessages);
   });
 
-  response = `Stylint: ${errorCount} Errors.`;
+  let response = `Stylint: ${errorCount} Errors.`;
   response += maxErrors ? ` (Max Errors: ${maxErrors} )` : '';
   response += `\nStylint: ${warningCount} Warnings.`;
   response += maxWarnings ? ` (Max Warnings: ${maxWarnings} )` : '';
