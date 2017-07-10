12ad32f5db1fea02a4157b0a1f1202c7d8e51373
Emma Bukacek <emma.bukacek@gmail.com>
2017-07-10 17:18:44 -0500

    Format default formatter to pass lint

--- src/formatters/default.js
+++ src/formatters/default.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const _ = require('lodash');
-
 /**
  * @description Default formatter for stylint.
  * @param  {Object} report - Report object containing all results.
@@ -17,44 +15,36 @@ const defaultFormatter = function(report, options, kill) {
   }
 
   const errorCount = report.errorCount;
+  const maxErrors = options.maxErrors;
+  const maxWarnings = options.maxWarnings;
   const warningCount = report.warningCount;
 
-  if (errorCount === 0 && warningCount === 0) {
-    return '';
-  }
-
-  const maxErrors = report.maxErrors;
-  const maxWarnings = report.maxWarnings;
-
-  const formattedMessages = report.results.map(result => {
+  let response = results.reduce((output, result) => {
     const filePath = result.filePath;
     const messages = result.messages;
 
-    const newMessages = messages.map(msg => {
-      const column = msg.column;
-      const line = msg.line;
-      const severity = msg.severity;
+    return output.concat(
+      messages
+        .map(msg => {
+          const column = msg.column;
+          const line = msg.line;
+          const severity = msg.severity;
+          const message = msg.message;
 
-      const lineData = column ? `${line}:${column}` : line;
+          const lineData = column ? `${line}:${column}` : line;
 
-      const newMessage = `${severity}: ${msg}
-        File: ${filePath}
-        Line: ${lineData}`;
+          return `${severity}: ${message}\nFile: ${filePath}\nLine: ${lineData}`;
+        })
+        .join('\n')
+    );
+  }, '');
 
-      return _.set(msg, 'message', newMessage);
-    });
-
-    return _.set(result, newMessages);
-  });
-
-  let response = `Stylint: ${errorCount} Errors.`;
-  response += maxErrors ? ` (Max Errors: ${maxErrors} )` : '';
+  response += `\n\nStylint: ${errorCount} Errors.`;
+  response += maxErrors ? ` (Max Errors: ${maxErrors})` : '';
   response += `\nStylint: ${warningCount} Warnings.`;
-  response += maxWarnings ? ` (Max Warnings: ${maxWarnings} )` : '';
-
+  response += maxWarnings ? ` (Max Warnings: ${maxWarnings})` : '';
   response += kill ? '\nStylint: Over Error or Warning Limit.' : '';
-
-  return `${formattedMessages}\n\n${response}`;
+  return response;
 };
 
 module.exports = defaultFormatter;
